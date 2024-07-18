fa602d262b6e2455bc17bef61fa4492492a9a7e2
shivamgupta02 <shivam.g@taltrip.com>
2024-07-18 16:08:33 +0530

    backend route of me,login and getDataFromToken

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,17 @@
+import { connect } from "@/dbConfig/dbConfig";
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+
+
+//connection to DB
+connect();
+
+export async function POST(request: NextRequest) {
+  // extract data from token
+  const userId = await getDataFromToken(request);
+  const user = User.findOne({ _id: userId }).select("-password");
+
+  //check if there is no  user
+  return NextResponse.json({ message: "User Found", data: user });
+}

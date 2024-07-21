742488fc2b8b1ed5485bb3c034df5391ea5f9d66
shivamgupta02 <shivam.g@taltrip.com>
2024-07-21 15:09:39 +0530

    login complete

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,71 @@
-import React from "react";
+"use client";
 
-const page = () => {
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
+
+export default function Login() {
+  const router = useRouter();
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const onLogin = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/login", user);
+      console.log("Login successful", response.data);
+      toast.success(response.data);
+      router.push("/profile");
+    } catch (error: any) {
+      console.log("Login failed");
+      toast.error(error.message);
+    }
+  };
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
   return (
     <>
-     login
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <h1>{loading ? "Processing..." : "Login Page"}</h1>
+        <hr />
+
+        {/* Email */}
+        <label htmlFor="email">Email : </label>
+        <input
+          className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+          type="text"
+          id="email"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+        />
+        {/* Password */}
+        <label htmlFor="Password">Password : </label>
+        <input
+          className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+          type="text"
+          id="Password"
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+        />
+        <button
+          onClick={onLogin}
+          className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        >
+          {buttonDisabled ? "Please fill the form completely" : "Login"}
+        </button>
+        <Link href="/signup">SignUp page</Link>
+      </div>
     </>
   );
-};
-
-export default page;
+}
